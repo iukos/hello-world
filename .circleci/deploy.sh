68fb4a451a8784149c2a57631af501c63f747986
xiaoweijia <xiaowei.jia@iukos.com>
2020-08-13 14:39:13 +0800

    - Update build script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,3 +6,9 @@ if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"
   kill -9 $PID
 fi
+
+PID=`ps -eaf | grep demo | grep -v grep | awk '{print $2}'`
+if [[ "" ==  "$PID" ]]; then
+  echo "starting demo"
+  java -jar demo-0.0.1-SNAPSHOT.jar &
+fi
