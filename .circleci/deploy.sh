65b74d0ee42cfbceb3832e0cbd1f882d2b7b3465
xiaoweijia <xiaowei.jia@iukos.com>
2020-08-14 13:49:07 +0800

    - Update build script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# My first script
+# Deploy jar script
 
+# 停止当前的服务
 PID=`ps -eaf | grep demo | grep -v grep | awk '{print $2}'`
 if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"
   kill -9 $PID
 fi
 
+# 删除现有的jar文件
+
+# 复制新的jar文件
+
+# 启动服务
 PID=`ps -eaf | grep demo | grep -v grep | awk '{print $2}'`
 if [[ "" ==  "$PID" ]]; then
   echo "starting demo"
